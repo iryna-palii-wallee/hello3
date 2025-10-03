7166504baf39958958ec9fdae3d335ba278b4aaf
Iryna Palii <iryna.paliy@wallee.com>
2025-10-03 15:37:29 +0200

    Initial commit
    Generated by rock@0.11.5.

---
+++ ios/Hello3React/Hello3React.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hello3React.
+FOUNDATION_EXPORT double Hello3ReactVersionNumber;
+
+//! Project version string for Hello3React.
+FOUNDATION_EXPORT const unsigned char Hello3ReactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hello3React/PublicHeader.h>
